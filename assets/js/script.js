0f8fd4582e0cf1316928ea84d1c29b4a89ecf7da
Luccas Faro <luccasfaro@hotmail.com>
2023-08-04 14:25:22 -0300

    feat: Alterações na parte visual do site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 var menuNavUl = document.querySelector('.menu-nav-header ul');
 var menuTrigger = document.querySelector('.menu-trigger');
+var menuTriggerIcon = document.querySelector('.menu-trigger i');
 
 menuTrigger.addEventListener("click", function menuShow() {
    if (menuNavUl.classList.contains('open')) {
       menuNavUl.classList.remove('open');
+      menuTriggerIcon.classList.remove('menu-open');
    } else {
       menuNavUl.classList.add('open');
+      menuTriggerIcon.classList.add('menu-open');
    }
 })
